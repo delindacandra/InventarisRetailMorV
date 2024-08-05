7fabb04f4776bf6d30135d0f5dde5710a31f5e51
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-08-05 07:50:49 +0700

    template blade

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,88 @@
+<div class="sidebar">
+    <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            {{-- <img src="images\logo_pertamina.png" class="img-circle elevation-2" alt="User Image"> --}}
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">Delinda Candrawati</a>
+        </div>
+    </div>
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+   with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="{{url('dashboard')}}" class="nav-link {{ ($activeMenu == 'dashboard')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-chart-bar"></i>
+                    <p>
+                        Dashboard
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-header">FLOW BARANG</li>
+            <li class="nav-item">
+                <a href="{{url('barang')}}" class="nav-link {{ ($activeMenu == 'data_barang')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-cubes"></i>
+                    <p>
+                        Data Barang
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{url('barang_baru')}}" class="nav-link {{ ($activeMenu == 'barang_baru')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-box"></i>
+                    <p>
+                        Barang Baru
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{url('barang_masuk')}}" class="nav-link {{ ($activeMenu == 'barang_masuk')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-truck-loading"></i>
+                    <p>
+                        Barang Masuk
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{url('barang_keluar')}}" class="nav-link {{ ($activeMenu == 'barang_keluar')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-truck-moving"></i>
+                    <p>
+                        Barang Keluar
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+            <li class="nav-header">TAMBAHAN</li>
+            <li class="nav-item">
+                <a href="{{url('user')}}" class="nav-link {{ ($activeMenu == 'user')? 'active' : '' }}">
+                    <i class="nav-icon fas fa-user"></i>
+                    <p>
+                        User
+                        {{-- <span class="right badge badge-danger">New</span> --}}
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inventory Retail Sales</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- untuk mengirimkan token laravel scrf pada setiap request -->
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('template/plugins/datatables-bs4/css/dataTables.bootsrap4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('template/plugins/datatables-responsive/css/responsive.bootsrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/plugins/datatables-buttons/css/buttons.bootsrap4.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+
+    @stack('css')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        @include('layouts.header')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-light-blue elevation-4">
+            <!-- Brand Logo -->
+            <a href="{{ url('dashboard') }}" class="brand-link">
+                <img src="images\logo_pertamina.png" alt="Logo" class="brand-image" style="opacity: .8">
+                <span class="brand-text font-weight-light">Inventory Retail Sales</span>
+            </a>
+
+            <!-- Sidebar -->
+            @include('layouts.sidebar')
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            @include('layouts.breadcrumb')
+
+            <!-- Main content -->
+            <section class="content">
+
+                @yield('content')
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        @include('layouts.footer')
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables $Plugins -->
+    <script src="{{ asset('template/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-responsive/js/responsive.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/datatables-buttons/js/buttons.colvis.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('template/dist/js/adminlte.min.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('template/dist/js/demo.js') }}"></script>
+    <script>
+        //untuk mengirimkan token laravel csrf pada setiap request ajax
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    @stack('js')<!-- Digunakan untuk memanggil custom js dari perintah push('js') pada masing view -->
+</body>
+
+</html>
