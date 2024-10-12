0e4bf77505f447998ce20922921a0f0514a2a29e
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-10-12 16:37:43 +0700

    perbaruhi data fungsi

--- database/seeders/FungsiSeeder.php
+++ database/seeders/FungsiSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -17,37 +16,37 @@ class FungsiSeeder extends Seeder
             [
                 'fungsi_id' => 1,
                 'kode_fungsi' => 'F01',
-                'nama_fungsi' => 'COS Region',
+                'nama_fungsi' => 'Executive GM',
             ],
             [
                 'fungsi_id' => 2,
                 'kode_fungsi' => 'F02',
-                'nama_fungsi' => 'Lubricant',
+                'nama_fungsi' => 'Corporate Sales',
             ],
             [
                 'fungsi_id' => 3,
                 'kode_fungsi' => 'F03',
-                'nama_fungsi' => 'IT Surabaya',
+                'nama_fungsi' => 'Corporate Opt & Srv',
             ],
             [
                 'fungsi_id' => 4,
                 'kode_fungsi' => 'F04',
-                'nama_fungsi' => 'RPD',
+                'nama_fungsi' => 'Retail Sales',
             ],
             [
                 'fungsi_id' => 5,
                 'kode_fungsi' => 'F05',
-                'nama_fungsi' => 'Internal Audit',
+                'nama_fungsi' => 'Supply & Distribution',
             ],
             [
                 'fungsi_id' => 6,
                 'kode_fungsi' => 'F06',
-                'nama_fungsi' => 'Commrel',
+                'nama_fungsi' => 'Rel & Proj. Dev.',
             ],
             [
                 'fungsi_id' => 7,
                 'kode_fungsi' => 'F07',
-                'nama_fungsi' => 'Retail',
+                'nama_fungsi' => 'HSSE',
             ],
             [
                 'fungsi_id' => 8,
@@ -57,93 +56,64 @@ class FungsiSeeder extends Seeder
             [
                 'fungsi_id' => 9,
                 'kode_fungsi' => 'F09',
-                'nama_fungsi' => 'Koperasi',
+                'nama_fungsi' => 'Asset Operation',
             ],
             [
                 'fungsi_id' => 10,
                 'kode_fungsi' => 'F10',
-                'nama_fungsi' => 'Pertalife',
+                'nama_fungsi' => 'Finance',
             ],
             [
                 'fungsi_id' => 11,
                 'kode_fungsi' => 'F11',
-                'nama_fungsi' => 'Finance',
+                'nama_fungsi' => 'HC',
             ],
             [
                 'fungsi_id' => 12,
                 'kode_fungsi' => 'F12',
-                'nama_fungsi' => 'HSSE',
+                'nama_fungsi' => 'Legal Counsel',
             ],
             [
                 'fungsi_id' => 13,
                 'kode_fungsi' => 'F13',
-                'nama_fungsi' => 'SSCT',
+                'nama_fungsi' => 'Comm, Rel, & CSR',
             ],
             [
                 'fungsi_id' => 14,
                 'kode_fungsi' => 'F14',
-                'nama_fungsi' => 'ICT',
+                'nama_fungsi' => 'Medical',
             ],
             [
                 'fungsi_id' => 15,
                 'kode_fungsi' => 'F15',
-                'nama_fungsi' => 'EGM',
+                'nama_fungsi' => 'IA',
             ],
             [
                 'fungsi_id' => 16,
                 'kode_fungsi' => 'F16',
-                'nama_fungsi' => 'PWP',
+                'nama_fungsi' => 'SSC ICT MOR V',
             ],
             [
                 'fungsi_id' => 17,
                 'kode_fungsi' => 'F17',
-                'nama_fungsi' => 'AFT Juanda',
+                'nama_fungsi' => 'Group Head Operation',
             ],
             [
                 'fungsi_id' => 18,
                 'kode_fungsi' => '18',
-                'nama_fungsi' => 'Legal',
+                'nama_fungsi' => 'SSC ICT VI',
             ],
             [
                 'fungsi_id' => 19,
                 'kode_fungsi' => 'F19',
-                'nama_fungsi' => 'Corp Sales',
+                'nama_fungsi' => 'Manager Sales Region V PT. PErtamina Lubricant',
             ],
             [
                 'fungsi_id' => 20,
                 'kode_fungsi' => 'F20',
-                'nama_fungsi' => 'HC',
+                'nama_fungsi' => 'GM Marine Business & Operation Region II',
             ],
-            [
-                'fungsi_id' => 21,
-                'kode_fungsi' => 'F21',
-                'nama_fungsi' => 'Asset',
-            ],
-            [
-                'fungsi_id' => 22,
-                'kode_fungsi' => 'F22',
-                'nama_fungsi' => 'S&D region',
-            ],
-            [
-                'fungsi_id' => 23,
-                'kode_fungsi' => 'F23',
-                'nama_fungsi' => 'Medical',
-            ],
-            [
-                'fungsi_id' => 24,
-                'kode_fungsi' => 'F24',
-                'nama_fungsi' => 'SA Retail Surabaya',
-            ],
-            [
-                'fungsi_id' => 25,
-                'kode_fungsi' => 'F25',
-                'nama_fungsi' => ' BPG',
-            ],
-            [
-                'fungsi_id' => 26,
-                'kode_fungsi' => 'F26',
-                'nama_fungsi' => 'PTK',
-            ],
+
         ];
         DB::table('fungsi_jatimbalinus')->insert($data);
     }
